199f86fc2df65a8b8e07fa76513df14189ca107a
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-10-12 16:29:04 +0530

    added program

--- array/duplicate.java
+++ array/duplicate.java
@@ -2,7 +2,7 @@ public class duplicate
 {
     public static void main(String[] args) 
     {
-        int []arr={1,2,3,2,4,3,7,6,6};
+       int[]arr={2,5,7,6,5,2,3,3};
         for(int i=0;i<arr.length;i++)
         {
             for(int j=i+1;j<arr.length;j++)
