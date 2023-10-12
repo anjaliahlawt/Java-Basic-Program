175c36fd55f230906a9932b4ec8aae33bf9a5dc1
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-10-12 16:19:10 +0530

    added program

---
+++ array/duplicate.java
@@ -0,0 +1,18 @@
+public class duplicate 
+{
+    public static void main(String[] args) 
+    {
+        int []arr={1,2,3,2,4,3,7,6,6};
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+    
+}
