f4d5282267bb6ba693f1ca7959fdfebdd05f9ff1
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-10-21 12:51:18 +0530

    added program

---
+++ BinarySearch/celing.java
@@ -0,0 +1,35 @@
+public class celing 
+{
+    public static void main(String[] args)
+    {
+       int[]arr={2,3,5,9,14,16,18};
+       int target=15;
+       int ans=search(arr,target);
+       System.out.println(ans);
+
+
+   }
+   static int search(int[]arr,int target)
+   {
+       int start=0;
+       int end=arr.length;
+       while(start<=end)
+       {
+          int mid=(start+end)/2;
+          if(target<arr[mid])
+          {
+              end=mid-1;
+          }
+          else if(target>arr[mid])
+          {
+             start=mid+1;
+          }
+          else
+          {
+             return mid;
+          }
+       }
+       return start;
+   }
+}
+ 
