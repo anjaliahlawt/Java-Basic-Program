6d2e8b1622ff7ec4eb301015e9cfe473d4f3e57c
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-12-26 19:59:22 +0530

    added program

--- class/typecasting.java
+++ class/typecasting.java
@@ -3,10 +3,23 @@
 public class typecasting 
 {
     public static void main(String[] args) 
+    // {
+    //     int n=3;
+    //     double d=n;
+    //     System.out.println(n);
+    //     System.out.println(d);
+    // }
+
+
+
+//narrowing casting
+
     {
-        int n=3;
-        double d=n;
+        
+        double dr=8.55d;
+        int n=(int)dr;
+
+        System.out.println(dr);
         System.out.println(n);
-        System.out.println(d);
     }
 }
