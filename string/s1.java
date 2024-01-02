98d771499578c2affbc79abf0fcb3f389f050785
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-01-02 11:02:14 +0530

    added program

---
+++ string/s1.java
@@ -0,0 +1,14 @@
+//package string;
+
+public class s1 
+{
+    public static void main(String[] args)
+     {
+        String name="anjali";
+        System.out.println(name);
+        System.out.println("The length of string is"+" "+name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.indexOf("n"));
+    }
+}
