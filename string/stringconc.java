2bb9d499715c8a200613954c32c458a4c5637616
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-01-02 13:20:08 +0530

    added program

---
+++ string/stringconc.java
@@ -0,0 +1,50 @@
+//package string;
+
+// public class stringconc 
+// {
+//     public static void main(String[] args) 
+//     {
+//         String firstName="anjali";
+//         String lastName="ahlawat";
+//         System.out.println("my name is"+firstName + lastName);
+//         System.out.println(firstName.concat(lastName));
+//     }
+// }
+
+
+//number and number add
+
+/*public class stringconc
+{
+   public static void main(String[] args)
+   {
+      int a=1;
+      int b=2;
+      System.out.println(a+b);
+
+   }
+}
+*/
+//string and number add
+/*public class stringconc
+{
+   public static void main(String[] args)
+   {
+     String a="10";
+     int b=2;
+     System.out.println(a+b);
+   }
+}
+*/
+
+//string and string add
+
+public class stringconc
+{
+   public static void main(String[] args)
+   {
+     String a="10";
+     String b="20";
+     System.out.println(a+b);
+   }
+}
