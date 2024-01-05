c2876032fb4c17e214dd1e2c68b2a1cca19c54dc
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-01-05 21:02:50 +0530

    added program

---
+++ encapsulation/enc.java
@@ -0,0 +1,43 @@
+//The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users
+
+//declare the class variable as a private. // declara tha class method as a public beacuse we access the class variable
+
+//package encapsulation;
+ 
+class student
+{
+   private int rollno;
+  
+   private String name;
+
+   public void setRollno(int a)
+   {
+      rollno=a;
+   }
+    public void setName(String b)
+   {
+      name=b;
+   }
+   public int getRollno()
+   {
+     return rollno;
+   }
+    public String getName()
+   {
+     return  name;
+   }
+
+
+
+}
+public class enc 
+{
+     public static void main(String[] args)
+    {
+        student obj=new student();
+        obj.setRollno(2);
+        obj.setName("anjali");
+        System.out.println("roolno is"+" "+obj.getRollno()+" "+"and name is"+" "+obj.getName()) ;   
+     }
+
+}
--- string/stringbuffer.java
+++ string/stringbuffer.java
@@ -7,7 +7,7 @@ public class stringbuffer
         name.append("ahlwat");
         name.delete(0,2);
         name.insert(0,'s');
-        name.replace('i','j');
+        name.replace(2,4,"nal");
         System.out.println(name);
     }
 }
