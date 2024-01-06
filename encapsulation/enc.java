cc87ea939f0f12e7acdd1a5616e4f5047dcc3e5e
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-01-06 14:09:25 +0530

    added program

--- encapsulation/enc.java
+++ encapsulation/enc.java
@@ -2,6 +2,8 @@
 
 //declare the class variable as a private. // declara tha class method as a public beacuse we access the class variable
 
+//the get method returns the variable value, and the set method sets the value.
+
 //package encapsulation;
  
 class student
@@ -11,24 +13,21 @@ class student
    private String name;
 
    public void setRollno(int a)
-   {
+    {
       rollno=a;
-   }
+    }
     public void setName(String b)
-   {
+    {
       name=b;
-   }
+    }
    public int getRollno()
-   {
+    {
      return rollno;
-   }
+    }
     public String getName()
-   {
+    {
      return  name;
-   }
-
-
-
+    }
 }
 public class enc 
 {
@@ -38,6 +37,6 @@ public class enc
         obj.setRollno(2);
         obj.setName("anjali");
         System.out.println("roolno is"+" "+obj.getRollno()+" "+"and name is"+" "+obj.getName()) ;   
-     }
+    }
 
 }
